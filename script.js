57f01f774f53b0301e6ca55dbf7759a0232f5501
brasil504 <brasilwahanda31@gmail.com>
2026-08-04 14:48:48 +0300

    Rename db variable to client in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const supabaseUrl = "https://hcscjoqebozfoeevfktl.supabase.co";
 const supabaseKey = "sb_publishable_AIew0nkWkEFAXTmLES-uqw_WqO_3vww";
 
-const db = window.supabase.createClient( supabaseUrl,supabaseKey);
+const client = supabase.createClient( supabaseUrl,supabaseKey);
 
 document.getElementById("signupForm").addEventListener("submit", async (event) => {
   event.preventDefault();
