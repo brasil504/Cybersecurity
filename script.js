fca4920801e4b645251854869ff91eaa697294b8
brasil504 <brasilwahanda31@gmail.com>
2026-08-04 14:14:14 +0300

    Implement user signup functionality with Supabase

---
+++ script.js
@@ -0,0 +1,30 @@
+const supabaseUrl = "https://hcscjoqebozfoeevfktl.supabase.co";
+const supabaseKey = "sb_publishable_AIew0nkWkEFAXTmLES-uqw_WqO_3vww";
+
+const db = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+document.getElementById("signupForm").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await db
+    .from("Users")
+    .insert([
+      {
+        email: email,
+        password: password
+      }
+    ]);
+
+  if (error) {
+    console.error(error);
+    alert("Something went wrong.");
+  } else {
+    alert("Registration successful!");
+  }
+});
